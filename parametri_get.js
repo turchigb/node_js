6c92e7417758fea9c674bdf851c45c18129cb997
turchigb <gbturchi@gbturchi.it>
2020-07-07 14:27:14 +0200

    Versione 1 per accettare parametri con GET

---
+++ parametri_get.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+
+var server = http.createServer(function(req, res) {
+  var params = querystring.parse(url.parse(req.url).query);
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  if ('firstname' in params && 'lastname' in params) {
+    res.write('Il tuo nome: ' + params['firstname'] + ' ' + params['lastname']);
+  } else {
+    res.write('Tu hai un nome giusto?');
+  }
+  res.end();
+});
+server.listen(8080);
